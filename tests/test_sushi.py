03c81f5506926a6b8d81f6e5137170c0aa3e664b
Alexandre Masse <alexandremasse2007@gmail.com>
2025-07-10 17:05:54 -0400

    Python tests 6

--- tests/test_sushi.py
+++ tests/test_sushi.py
@@ -1,6 +1,5 @@
 import os
 
-import pytest
 from dotenv import load_dotenv
 from web3 import Web3
 from web3.exceptions import ContractLogicError
@@ -17,8 +16,6 @@ WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 USDC = "0xA0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
 AMOUNT = Web3.to_wei(1, "ether")
 
-pytestmark = pytest.mark.asyncio  # Applique asyncio à toutes les fonctions ici
-
 
 async def test_uniswap_price():
     adapter = SushiAdapter(web3)
@@ -26,39 +23,3 @@ async def test_uniswap_price():
     assert price is not None
     assert price > 0
     print(f"Uniswap price: {price:.2f} USDC")
-
-
-async def test_sushi_invalid_token_addresses():
-    adapter = SushiAdapter(web3)
-    invalid_token = "0x0000000000000000000000000000000000000000"
-    with pytest.raises(Exception):
-        await adapter.get_price(invalid_token, invalid_token, 1000)
-
-
-async def test_sushi_zero_amount():
-    adapter = SushiAdapter(web3)
-    WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-    USDC = "0xA0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
-    amount = 0
-
-    with pytest.raises(ContractLogicError) as excinfo:
-        await adapter.get_price(WETH, USDC, amount)
-
-    assert "INSUFFICIENT_INPUT_AMOUNT" in str(excinfo.value)
-
-
-async def test_sushi_large_amount():
-    adapter = SushiAdapter(web3)
-    WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-    USDC = "0xA0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
-    large_amount = 10**30
-    price = await adapter.get_price(WETH, USDC, large_amount)
-    assert price is None or price > 0
-
-
-async def test_sushi_unsupported_pair():
-    adapter = SushiAdapter(web3)
-    FAKE_TOKEN = "0x1111111111111111111111111111111111111111"
-    WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-    with pytest.raises(Exception):
-        await adapter.get_price(FAKE_TOKEN, WETH, 1000)
